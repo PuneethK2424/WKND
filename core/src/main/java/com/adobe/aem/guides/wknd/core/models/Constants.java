6faece3eba2c30578c0cb6507b044c2667caccc4
Murali-295 <murali.kolli@nextrow.com>
2025-05-21 16:21:10 +0530

    creating userNode before creating playlist

--- core/src/main/java/com/adobe/aem/guides/wknd/core/models/Constants.java
+++ core/src/main/java/com/adobe/aem/guides/wknd/core/models/Constants.java
@@ -1,5 +1,5 @@
 package com.adobe.aem.guides.wknd.core.models;
 
 public class Constants {
-    public static final String ROOT_FOLDER_PATH ="/content/user-playlists";
+    public static final String ROOT_FOLDER_PATH ="/conf/user-playlists";
 }
